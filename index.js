4041b37628f00d5b07501712e98f8763a9f4c41a
Bubbline mint <mariusz.borczuk@edu.uekat.pl>
2024-02-29 19:54:43 +0100

    Update HTML and CSS files

---
+++ index.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonPets = document.querySelector('.Pets');
+  const buttonKawaiiSteak = document.querySelector('.KawaiiSteak');
+  const buttonPicture1 = document.querySelector('.Picture1');
+  const buttonPicture2 = document.querySelector('.Picture2');
+  const buttonPicture3 = document.querySelector('.Picture3');
+  const buttonPicture4 = document.querySelector('.Picture4');
+  const buttonPicture5 = document.querySelector('.Picture5');
+  const buttonPicture6 = document.querySelector('.Picture6');
+  const buttonSearch = document.querySelector('.Search');
+  const buttonLogOut = document.querySelector('.LogOut');
+  const buttonContact = document.querySelector('.Contact');
+  const buttonPets3 = document.querySelector('.Pets3');
+  const buttonSupport = document.querySelector('.Support');
+
+  buttonPets.addEventListener('click', () => {
+    console.log('Button clicked!');
+  });
+});
