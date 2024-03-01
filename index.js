882022ccade537355e8011b53e5f4b5bb364d061
Mariusz Borczuk <116025282+Mariusz-Borczuk@users.noreply.github.com>
2024-03-01 16:41:53 +0100

    Refactor button event listeners and update image paths

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-const profileButton1 = document.getElementById('FirstProf');
-const profileButton2 = document.getElementById('SecondProf');
-const profileButton3 = document.getElementById('ThirdProf');
-const profileButton4 = document.getElementById('FourthProf');
-const profileButton5 = document.getElementById('FifthProf');
-const profileButton6 = document.getElementById('SixthProf');
+const profileButton1 = document.getElementById('FirstProf');//✔️
+const profileButton2 = document.getElementById('SecondProf');//✔️
+const profileButton3 = document.getElementById('ThirdProf');//✔️
+const profileButton4 = document.getElementById('FourthProf');//✔️
+const profileButton5 = document.getElementById('FifthProf');//✔️
+const profileButton6 = document.getElementById('SixthProf');//✔️
+//
+const searchButton = document.getElementById('Search');//✔️
+//
+const ourAnimalsButton = document.getElementById('Ourpets');//✔️
+const supportButton = document.getElementById('SupportUs');//✔️
+const contactButton = document.getElementById('ContactUs');//✔️
+const logoutButton = document.getElementById('LogUt');//✔️
+//
+const foodButton = document.getElementById('Food');//✔️
+const pettsButton = document.getElementById('Pets');//✔️
+//Account
+const accountButton = document.getElementById('MyAccount');//✔️
 
+//Profiles
 profileButton1.addEventListener('click', () => {
-  console.log('First Profile');
-});
+  console.log('First Profile');});
 profileButton2.addEventListener('click', () => {
-  console.log('Second Profile');
-});
+  console.log('Second Profile');});
 profileButton3.addEventListener('click', () => {
-  console.log('Third Profile');
-});
+  console.log('Third Profile');});
 profileButton4.addEventListener('click', () => {
-  console.log('Fourth Profile');
-});
+  console.log('Fourth Profile');});
 profileButton5.addEventListener('click', () => {
-  console.log('Fifth Profile');
-});
+  console.log('Fifth Profile');});
 profileButton6.addEventListener('click', () => {
   console.log('Sixth Profile');
+});
+//search
+searchButton.addEventListener('click', () => {
+  console.log('Search');
+});
+//top menu
+ourAnimalsButton.addEventListener('click', () => {
+  console.log('OurAnimals');
+});
+contactButton.addEventListener('click', () => {
+  console.log('ContactUs');
+});
+supportButton.addEventListener('click', () => {
+  console.log('SupportUs');
+});
+logoutButton.addEventListener('click', () => {
+  console.log('LogOut');
+});
+
+//side menu
+foodButton.addEventListener('click', () => {
+  console.log('Food');
+});
+pettsButton.addEventListener('click', () => {
+  console.log('Pets');
+});
+//Account
+accountButton.addEventListener('click', () => {
+  console.log('MyAccount');
 });
