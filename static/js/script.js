cf79c242159daa22081e83761e02ead7e62162ed
Filip Paryż <paryz.filip@gmail.com>
2024-04-07 16:26:50 +0200

    Added management page for shelters

--- static/js/script.js
+++ static/js/script.js
@@ -3,27 +3,31 @@ onload = function() {
 };
 
 function loadOnClicks(){
-  const animal = document.getElementById('Animal1');
-  const searchButton = document.getElementById('search-button');
+  // Menu buttons
   const ourAnimalsButton = document.getElementById('Ourpets');
   const supportButton = document.getElementById('SupportUs');
+  const shelterButton = document.getElementById('shelters-button');
   const logoutButton = document.getElementById('LogUt');
-  //Profiles
-  animal.addEventListener('click', () => {
-    console.log('Animal');
-  });
-  //search
-  searchButton.addEventListener('click', () => {
-    console.log('Search');
-  });
-  //top menu
   ourAnimalsButton.addEventListener('click', () => {
     window.location.href = manageAnimalsUrl;
+  });
+  supportButton.addEventListener('click', () => {
+    window.location.href = supportUsUrl;
+  });
+  shelterButton.addEventListener('click', () => {
+    window.location.href = manageSheltersUrl;
   });
   logoutButton.addEventListener('click', () => {
     console.log('LogOut');
   });
-  supportButton.addEventListener('click', () => {
-      window.location.href = supportUsUrl;
+
+  // Animal profiles
+  const animal = document.getElementById('Animal1');
+  const searchButton = document.getElementById('search-button');
+  animal.addEventListener('click', () => {
+    console.log('Animal');
+  });
+  searchButton.addEventListener('click', () => {
+    console.log('Search');
   });
 }
